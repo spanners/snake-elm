669e708fd48fc0b7dec0b569211440630c4be232
Simon Buist <simon.buist@gmail.com>
2014-03-08 18:54:53 -0500

    Add type annotations for every function

--- Main.elm
+++ Main.elm
@@ -3,17 +3,23 @@ import Keyboard
 
 (width, height) = (600, 400)
 (hWidth, hHeight) = (width/2, height/2)
+
+speed : Float
 speed = 5
 
 data Event = Tick (Time, (Int, Int))
 
-tf : Float -> Float -> String -> Form
-tf y scl str = toText str |> Text.color black
+formString : Float -> Float -> String -> Form
+formString y scl str = toText str |> Text.color black
                           |> text 
                           |> toForm
                           |> scale scl 
                           |> move (0, y)
+
+delta : Signal Float
 delta = (fps 30)
+
+input : Signal (Float, (Int, Int))
 input = (,) <~ lift inSeconds delta
              ~ sampleOn delta ((\{ x, y } -> (x, y)) <~ Keyboard.arrows)
 
@@ -21,47 +27,70 @@ type Vec = (Float, Float)
 
 data Heading = Up | Down | Left | Right
 
-type Snake = {pos:Vec, heading:Heading}
+type Snake = { pos     : Vec
+             , heading : Heading
+             , body    : [Vec] }
 
-defaultSnake = { pos = (0, 0),
-                 heading = Right }
+defaultSnake : Snake
+defaultSnake = { pos = (0, 0)
+               , heading = Right 
+               , body = [(5, 0)] }
 
 type Game = { snake : Snake
             , score : Int }
 
+defaultGame : Game
 defaultGame = { snake = defaultSnake
               , score = 0 }
 
-left dir = (fst dir) < 0
-right dir = (fst dir) > 0
-down dir = (snd dir) < 0
-up dir = (snd dir) > 0
+updatePos : Heading -> Vec -> Vec
+updatePos h (x,y) = if | h == Left -> (x - speed, y)
+          | h == Right -> (x + speed, y)
+          | h == Down -> (x, y - speed)  
+          | h == Up -> (x, y + speed)        
+          | otherwise -> (x, y)
 
-stepSnake dir snake = { snake | pos <- if | snake.heading == Left -> ((fst snake.pos) - speed, (snd snake.pos))
-                                          | snake.heading == Right -> ((fst snake.pos) + speed, (snd snake.pos))
-                                          | snake.heading == Down -> ((fst snake.pos), (snd snake.pos) - speed)
-                                          | snake.heading == Up -> ((fst snake.pos), (snd snake.pos) + speed)
-                                          | otherwise -> snake.pos
-                              , heading <- if | left dir -> if snake.heading == Left || snake.heading == Right then snake.heading else Left 
-                                              | right dir -> if snake.heading == Left || snake.heading == Right then snake.heading else Right 
-                                              | down dir -> if snake.heading == Up || snake.heading == Down then snake.heading else Down
-                                              | up dir -> if snake.heading == Up || snake.heading == Down then snake.heading else Up
-                                              | otherwise -> snake.heading }
+stepSnake : (Int, Int) -> Snake -> Snake
+stepSnake dir ({pos, heading} as snake) = 
+    let h = heading
+        left dir = (fst dir) < 0
+        right dir = (fst dir) > 0
+        down dir = (snd dir) < 0
+        up dir = (snd dir) > 0
+        getHeading dir = if | left dir -> Left
+                            | right dir -> Right
+                            | down dir -> Down
+                            | up dir -> Up
+    in { snake | pos <- updatePos h pos
+               , heading <- if | left dir || right dir -> if h == Left || h == Right 
+                                                          then h 
+                                                          else getHeading dir
+                               | down dir || up dir -> if h == Up || h == Down 
+                                                       then h 
+                                                       else getHeading dir
+                               | otherwise -> h
+               , body <- [pos] }
 
+stepGame : Event -> Game -> Game
 stepGame event g = 
     case event of
-        Tick (t, dir) -> let g' = { g | snake <- stepSnake dir g.snake, score <- g.score + 1 }
-                             out = let (x, y) = g.snake.pos in abs x > hWidth || abs y > hHeight
+        Tick (t, dir) -> let g' = { g | snake <- stepSnake dir g.snake
+                                      , score <- g.score + 1 }
+                             out = let (x, y) = g.snake.pos 
+                                   in abs x > hWidth || abs y > hHeight
                          in if out then defaultGame else g'
         _             -> g
 
+event : Signal Event
 event = merges [ lift Tick input ]
 
+render : (Int, Int) -> Game -> Element
 render (w, h) g = 
-    let formSnake {pos} = circle 10 |> filled black |> move pos
-        txts  = [ (tf 0 4 (show g.snake.heading)), (tf 50 2 (show g.snake.pos)) ]
-        forms = txts ++ [formSnake g.snake]
+    let formSnake pos = circle 10 |> filled black |> move pos
+        txts  = [ (formString 0 2 (show g.snake.heading))
+                , (formString 50 2 (show g.snake.pos)) ]
+        forms = txts ++ map formSnake (g.snake.pos :: g.snake.body)
     in color black <| container w h middle <| color white <| collage width height forms
 
+main : Signal Element
 main = render <~ Window.dimensions ~ foldp stepGame defaultGame event
-
